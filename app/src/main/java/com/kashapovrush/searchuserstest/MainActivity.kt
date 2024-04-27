7b13744223f307e3083db7460d9b2c879b53be7f
rushankasapov <kashapovrush@mail.ru>
2024-04-27 14:35:07 +0500

    implementation auth

--- app/src/main/java/com/kashapovrush/searchuserstest/MainActivity.kt
+++ app/src/main/java/com/kashapovrush/searchuserstest/MainActivity.kt
@@ -15,13 +15,8 @@ class MainActivity : AppCompatActivity(), SearchUsersFragment.ClickListenerFromS
     override fun onCreate(savedInstanceState: Bundle?) {
         binding = ActivityMainBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
-//        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-//            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-//            insets
-//        }
+
 
             supportFragmentManager.commit {
                 replace(R.id.container, AuthFragment.newInstance())
